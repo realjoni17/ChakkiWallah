f467a9e1c132fc2bc0477d38bcb458eb806187f9
realjoni17 <jaunivashisth@gmail.com>
2023-04-23 12:18:33 +0530

    first commit

---
+++ app/src/main/java/com/android/chakkiwallah/presentation/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.android.chakkiwallah.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.android.chakkiwallah.presentation.ui.theme.ChakkiWallahTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ChakkiWallahTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ChakkiWallahTheme {
+        Greeting("Android")
+    }
+}
